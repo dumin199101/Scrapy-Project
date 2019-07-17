8abdc07aa956ee25468fbb3c04680d66c4cbd06d
dumin199101 <dumin199101@163.com>
2019-07-17 18:24:50 +0800

    更新middlewares.py

--- MaoCuHuiSpider/MaoCuHuiSpider/middlewares.py
+++ MaoCuHuiSpider/MaoCuHuiSpider/middlewares.py
@@ -60,7 +60,7 @@ class MaocuhuicrawlspiderSpiderMiddleware(object):
         spider.logger.info('Spider opened: %s' % spider.name)
 
 
-class MaocuhuispiderDownloaderMiddleware(object):
+class MaocuhuicrawlspiderDownloaderMiddleware(object):
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the downloader middleware does not modify the
     # passed objects.
